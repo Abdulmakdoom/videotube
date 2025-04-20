77fe6b9e6a8984c6deed05f82d9911512f1b2afa
Abdulmakdoom <abdulmakdoom90@gmail.com>
2025-04-20 21:13:01 +0530

    fix: update CORS headers and adjust Vite proxy configuration for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cors({
 // }));
 
 // app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://videotube-.vercel.app");
+//     res.header("Access-Control-Allow-Origin", "https://videotube-bice.vercel.app");
 //     // res.header("Access-Control-Allow-Credentials", "true");
 //     // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 //     //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
