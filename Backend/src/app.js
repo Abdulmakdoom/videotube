8b1ffbcad67e9e5d5885f9bc07f4840de3e21d78
Abdulmakdoom <abdulmakdoom90@gmail.com>
2025-04-21 16:56:45 +0530

    add

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors"; 
 import cookieParser from "cookie-parser"; 
+import path from "path";
+
 
 
 const app = express()
@@ -36,7 +38,7 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'build'))); // or 'public' depending on your setup
+
 
 // app.use('/', (req, res)=>{
 //     res.send("Done")
