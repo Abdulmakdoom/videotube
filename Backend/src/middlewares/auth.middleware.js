232bb1132b66175002b7ed0dac34e6e0dff04a71
Abdulmakdoom <abdulmakdoom90@gmail.com>
2025-05-13 23:27:46 +0530

    fix: enable token logging in JWT middleware for debugging purposes

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -12,8 +12,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         // const token = req.cookies?.accessToken
         // const token = (req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "") || "").trim();
         const token = (req.header("Authorization")?.replace("Bearer ", "") || "").trim();
-
-        //console.log(token);
+        console.log(token);
         
 
         // console.log(token);
